4165b3b8ab8cde2823120ae2efcf4948114e0ccf
KevinModernel <114360790+KevinModernel@users.noreply.github.com>
2022-12-22 18:43:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import { sequelize } from './database/database.js';
 
 
 async function main() {
-	try{ // .sync method is to synchronize your Sequelize model with your database tables.
-		await sequelize.sync({force: false}); //Crea las tablas si no existen. Con el force:true borra lo que hay y vuelve a crear. Le pongo force: false para que no vuelva a crear las tablas cada vez que se vuelve a ejecutar el sv por nodemon.
-		// await sequelize.authenticate(); // para probar que sequelize funciona. Funciona Ok.
+	try{ 
+		await sequelize.sync({force: false});
 		app.listen(3000); 
 		console.log('Server is listening port 3000');
 	} catch (e) {
@@ -14,6 +13,3 @@ async function main() {
 };
 
 main();
-
-
-// Te podes conectar a sequelize

be993923bcd44d60ad57d8608b5e08d149f40a36
KevinModernel <114360790+KevinModernel@users.noreply.github.com>
2022-12-22 18:44:42 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,4 @@
 import Sequelize from 'sequelize';
-/// To connect to the database, you must create a Sequelize instance.
-/// Option 3: Passing parameters separately (other dialects)
-//
-///const sequelize = new Sequelize('database', 'username', 'password', {
-///  host: 'localhost',
-///  dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-///});
 
 export const sequelize =  new Sequelize(
 	'projectsdb', 
